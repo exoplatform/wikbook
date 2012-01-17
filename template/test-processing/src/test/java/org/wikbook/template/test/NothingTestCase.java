f610055f70030829353b5dbbb21c37600f3348cb
Alain Defrance <defrancea@gmail.com>
2012-01-17 16:39:05 +0100

    Test no processed class

--- template/test-processing/src/test/java/org/wikbook/template/test/NothingTestCase.java
+++ template/test-processing/src/test/java/org/wikbook/template/test/NothingTestCase.java
@@ -2,23 +2,26 @@ package org.wikbook.template.test;
 
 import org.wikbook.template.processing.metamodel.MetaModel;
 
+import java.io.FileNotFoundException;
+
 /**
  * @author <a href="mailto:alain.defrance@exoplatform.com">Alain Defrance</a>
  * @version $Revision$
  */
 public class NothingTestCase extends AbstractProcessorTestCase {
 
-  /*public void testA() throws Exception {
+  public void testA() throws Exception {
 
     try {
       buildClass("A");
+      readMetaModel("model.A.src");
       fail();
     }
     catch (FileNotFoundException e) {
-      // no processing done
+      // no processing donen it's ok
     }
     
-  }*/
+  }
 
   public void testB() throws Exception {
 
